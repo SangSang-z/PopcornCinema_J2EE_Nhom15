3eaa2322e242def417469e2bd2bc5e3a798c6675
SangSang-z <sangsangks1903@gmail.com>
2026-03-03 21:59:46 +0700

    chore: add .gitattributes for line ending normalization

---
+++ src/test/java/com/example/PopcornCinema/PopcornCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PopcornCinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
