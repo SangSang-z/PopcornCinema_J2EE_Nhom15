45144332ff0e3f388c1cd82a0e81e0081abaffcc
Tran Nguyen Ai Nhi <ainhitran2003@gmail.com>
2026-03-10 14:44:36 +0700

    feat(home): update home controller, banner slider and layout

---
+++ src/main/resources/static/js/slider.js
@@ -0,0 +1,18 @@
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index){
+    slides.forEach(slide=>{
+        slide.classList.remove("active");
+    });
+    slides[index].classList.add("active");
+}
+
+function nextSlide(){
+    currentSlide++;
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+    showSlide(currentSlide);
+}
+setInterval(nextSlide,4000);
